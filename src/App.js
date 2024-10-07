44ef7189bd911e754c4102c0f58171a1c2535d78
chamika-damith <chamikadamith9@gmail.com>
2024-10-07 22:37:53 +0530

    update navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ItemsPage from './ItemsPage';
 import Layout from './Layout';
 import POSPage from './POSPage';
 import { Navigate, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
@@ -12,6 +13,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Navigate to="/pos" />} />
             <Route path="/pos" element={<POSPage />} />
+            <Route path="/ItemsPage" element={<ItemsPage />} />
           </Routes>
         </div>
       </Layout>
